5d93667f905056f34cd859eb55be55f3712a0a57
adalbertofjr <adalberto.fernandesjr@gmail.com>
2018-01-20 15:31:24 -0200

    task: Adicionando módulo Wear e dados para o dispositivo wear.
    Módulo: Seja-Onipresente - Nanodegree

---
+++ Wearable/src/main/java/com/example/android/sunshine/util/SunshineWatchFaceUtil.java
@@ -0,0 +1,173 @@
+package com.example.android.sunshine.util;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.sunshine.watchface.SunshineWatchFaceService;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.Wearable;
+
+/**
+ * SunshineWatchFaceUtil
+ * Created by Adalberto Fernandes Júnior on 19/11/17.
+ * Copyright © 2016. All rights reserved.
+ */
+
+public class SunshineWatchFaceUtil {
+    private static String LOG_TAG = SunshineWatchFaceUtil.class.getSimpleName();
+
+    /**
+     * The {@link DataMap} key for {@link SunshineWatchFaceService} background color name.
+     * The color name must be a {@link String} recognized by {@link Color#parseColor}.
+     */
+    public static final String KEY_BACKGROUND_COLOR = "BACKGROUND_COLOR";
+
+    /**
+     * The {@link DataMap} key for {@link SunshineWatchFaceService} hour digits color name.
+     * The color name must be a {@link String} recognized by {@link Color#parseColor}.
+     */
+    public static final String KEY_HOURS_COLOR = "HOURS_COLOR";
+
+    /**
+     * The {@link DataMap} key for {@link SunshineWatchFaceService} minute digits color name.
+     * The color name must be a {@link String} recognized by {@link Color#parseColor}.
+     */
+    public static final String KEY_MINUTES_COLOR = "MINUTES_COLOR";
+
+    /**
+     * The {@link DataMap} key for {@link SunshineWatchFaceService} second digits color name.
+     * The color name must be a {@link String} recognized by {@link Color#parseColor}.
+     */
+    public static final String KEY_SECONDS_COLOR = "SECONDS_COLOR";
+
+    /**
+     * The path for the {@link DataItem} containing {@link SunshineWatchFaceService} configuration.
+     */
+    public static final String PATH_WITH_FEATURE = "/watch_face_config/Digital";
+
+
+    /**
+     * Name of the default interactive mode background color and the ambient mode background color.
+     */
+    public static final String COLOR_NAME_DEFAULT_AND_AMBIENT_BACKGROUND = "Blue";
+    public static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_BACKGROUND =
+            parseColor(COLOR_NAME_DEFAULT_AND_AMBIENT_BACKGROUND);
+
+    /**
+     * Name of the default interactive mode hour digits color and the ambient mode hour digits
+     * color.
+     */
+    public static final String COLOR_NAME_DEFAULT_AND_AMBIENT_HOUR_DIGITS = "White";
+    public static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_HOUR_DIGITS =
+            parseColor(COLOR_NAME_DEFAULT_AND_AMBIENT_HOUR_DIGITS);
+
+    /**
+     * Name of the default interactive mode minute digits color and the ambient mode minute digits
+     * color.
+     */
+    public static final String COLOR_NAME_DEFAULT_AND_AMBIENT_MINUTE_DIGITS = "White";
+    public static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_MINUTE_DIGITS =
+            parseColor(COLOR_NAME_DEFAULT_AND_AMBIENT_MINUTE_DIGITS);
+
+    /**
+     * Name of the default interactive mode second digits color and the ambient mode second digits
+     * color.
+     */
+    public static final String COLOR_NAME_DEFAULT_AND_AMBIENT_SECOND_DIGITS = "Gray";
+    public static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_SECOND_DIGITS =
+            parseColor(COLOR_NAME_DEFAULT_AND_AMBIENT_SECOND_DIGITS);
+
+
+    private static int parseColor(String colorName) {
+        return Color.parseColor(colorName.toLowerCase());
+    }
+
+    /**
+     * Asynchronously fetches the current config {@link DataMap} for {@link SunshineWatchFaceService}
+     * and passes it to the given callback.
+     * <p>
+     * If the current config {@link DataItem} doesn't exist, it isn't created and the callback
+     * receives an empty DataMap.
+     */
+    public static void fetchConfigDataMap(final GoogleApiClient client,
+                                          final FetchConfigDataMapCallback callback) {
+        Wearable.NodeApi.getLocalNode(client).setResultCallback(
+                new ResultCallback<NodeApi.GetLocalNodeResult>() {
+                    @Override
+                    public void onResult(NodeApi.GetLocalNodeResult getLocalNodeResult) {
+                        String localNode = getLocalNodeResult.getNode().getId();
+                        Uri uri = new Uri.Builder()
+                                .scheme("wear")
+                                .path(SunshineWatchFaceUtil.PATH_WITH_FEATURE)
+                                .authority(localNode)
+                                .build();
+                        Wearable.DataApi.getDataItem(client, uri)
+                                .setResultCallback(new DataItemResultCallback(callback));
+                    }
+                }
+        );
+    }
+
+    /**
+     * Callback interface to perform an action with the current config {@link DataMap} for
+     * {@link SunshineWatchFaceService}.
+     */
+    public interface FetchConfigDataMapCallback {
+        /**
+         * Callback invoked with the current config {@link DataMap} for
+         * {@link SunshineWatchFaceService}.
+         */
+        void onConfigDataMapFetched(DataMap config);
+    }
+
+    /**
+     * Overwrites the current config {@link DataItem}'s {@link DataMap} with {@code newConfig}.
+     * If the config DataItem doesn't exist, it's created.
+     */
+    public static void putConfigDataItem(GoogleApiClient googleApiClient, DataMap newConfig) {
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(PATH_WITH_FEATURE);
+        putDataMapRequest.setUrgent();
+        DataMap configToPut = putDataMapRequest.getDataMap();
+        configToPut.putAll(newConfig);
+        Wearable.DataApi.putDataItem(googleApiClient, putDataMapRequest.asPutDataRequest())
+                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                    @Override
+                    public void onResult(DataApi.DataItemResult dataItemResult) {
+                        if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+                            Log.d(LOG_TAG, "putDataItem result status: " + dataItemResult.getStatus());
+                        }
+                    }
+                });
+    }
+
+    private static class DataItemResultCallback implements ResultCallback<DataApi.DataItemResult> {
+
+        private final FetchConfigDataMapCallback mCallback;
+
+        public DataItemResultCallback(FetchConfigDataMapCallback callback) {
+            mCallback = callback;
+        }
+
+        @Override
+        public void onResult(DataApi.DataItemResult dataItemResult) {
+            if (dataItemResult.getStatus().isSuccess()) {
+                if (dataItemResult.getDataItem() != null) {
+                    DataItem configDataItem = dataItemResult.getDataItem();
+                    DataMapItem dataMapItem = DataMapItem.fromDataItem(configDataItem);
+                    DataMap config = dataMapItem.getDataMap();
+                    mCallback.onConfigDataMapFetched(config);
+                } else {
+                    mCallback.onConfigDataMapFetched(new DataMap());
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -19,7 +19,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -36,12 +35,7 @@ import android.widget.ProgressBar;
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.sync.SunshineSyncUtils;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wearable.DataClient;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataItem;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
@@ -49,7 +43,7 @@ import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor>,
-        ForecastAdapter.ForecastAdapterOnClickHandler, DataClient.OnDataChangedListener {
+        ForecastAdapter.ForecastAdapterOnClickHandler{
 
     private final String TAG = MainActivity.class.getSimpleName();
 
@@ -169,17 +163,11 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Instantiates clients without member variables, as clients are inexpensive to create and
-        // won't lose their listeners. (They are cached and shared between GoogleApi instances.)
-        Wearable.getDataClient(this).addListener(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        Wearable.getDataClient(this).removeListener(this);
     }
 
     /**
@@ -291,10 +279,11 @@ public class MainActivity extends AppCompatActivity implements
      */
     @Override
     public void onClick(long date) {
-//        Intent weatherDetailIntent = new Intent(MainActivity.this, DetailActivity.class);
-//        Uri uriForDateClicked = WeatherContract.WeatherEntry.buildWeatherUriWithDate(date);
-//        weatherDetailIntent.setData(uriForDateClicked);
-//        startActivity(weatherDetailIntent);
+        Intent weatherDetailIntent = new Intent(MainActivity.this, DetailActivity.class);
+        Uri uriForDateClicked = WeatherContract.WeatherEntry.buildWeatherUriWithDate(date);
+        weatherDetailIntent.setData(uriForDateClicked);
+        startActivity(weatherDetailIntent);
+
         sendData();
     }
 
@@ -368,23 +357,6 @@ public class MainActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onDataChanged(@NonNull DataEventBuffer dataEvents) {
-        Log.i(TAG, "onDataChanged: " + dataEvents);
-
-        for (DataEvent event : dataEvents) {
-            if (event.getType() == DataEvent.TYPE_CHANGED) {
-                Log.i(TAG, "TYPE_CHANGED: " + event.getDataItem().toString());
-//                mDataItemListAdapter.add(
-//                        new Event("DataItem Changed", event.getDataItem().toString()));
-            } else if (event.getType() == DataEvent.TYPE_DELETED) {
-                Log.i(TAG, "TYPE_DELETED: " + event.getDataItem().toString());
-//                mDataItemListAdapter.add(
-//                        new Event("DataItem Deleted", event.getDataItem().toString()));
-            }
-        }
-
-    }
 
     private static final String SUNSHINE_PATH = "/sunshine";
     private static final String IMAGE_PATH = "/image";
@@ -392,58 +364,17 @@ public class MainActivity extends AppCompatActivity implements
     private static final String MIN_KEY = "min";
 
     public void sendData() {
-        new DataItemSend().run();
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(SUNSHINE_PATH);
+        putDataMapRequest.getDataMap().putInt(MAX_KEY, 25);
+        putDataMapRequest.getDataMap().putInt(MIN_KEY, 16);
+
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+        request.setUrgent();
+
+        Log.i(TAG, "Generating DataItem: " + request);
+
+        Task<DataItem> dataItemTask =
+                Wearable.getDataClient(getApplicationContext()).putDataItem(request);
     }
 
-    /**
-     * Generates a DataItem based on an incrementing count.
-     */
-    private class DataItemSend implements Runnable {
-
-        @Override
-        public void run() {
-            Log.i(TAG, "sendData()");
-
-            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(SUNSHINE_PATH);
-            putDataMapRequest.getDataMap().putString(MAX_KEY, "25º");
-            putDataMapRequest.getDataMap().putString(MIN_KEY, "16º");
-
-            PutDataRequest request = putDataMapRequest.asPutDataRequest();
-            request.setUrgent();
-
-            Log.i(TAG, "Generating DataItem: " + request);
-
-            Task<DataItem> dataItemTask =
-                    Wearable.getDataClient(getApplicationContext()).putDataItem(request);
-
-            dataItemTask.addOnSuccessListener(new OnSuccessListener<DataItem>() {
-                @Override
-                public void onSuccess(DataItem dataItem) {
-                    Log.i(TAG, "Sending sunshine data was successful: " + dataItem);
-                }
-            });
-
-            dataItemTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.i(TAG, "Sending failure to sunshine data was successful: " + e.getMessage());
-                }
-            });
-
-
-//            try {
-//                // Block on a task and get the result synchronously (because this is on a background
-//                // thread).
-//                DataItem dataItem = Tasks.await(dataItemTask);
-//
-//                LOGD(TAG, "DataItem saved: " + dataItem);
-//
-//            } catch (ExecutionException exception) {
-//                Log.e(TAG, "Task failed: " + exception);
-//
-//            } catch (InterruptedException exception) {
-//                Log.e(TAG, "Interrupt occurred: " + exception);
-//            }
-        }
-    }
 }
